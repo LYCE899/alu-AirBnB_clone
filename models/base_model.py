b9116300a9a607e758b281160a7489f07e91c37f
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-05-18 22:08:42 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ class BaseModel:
 
     """Initialise class"""
 
-    def __init__self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
