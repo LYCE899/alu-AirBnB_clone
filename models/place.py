f1d7e2a4734fadf587e1ef67caff4df0ac8a2397
LYCE899 <a.coulibaly@alustudent.com>
2023-05-18 18:16:32 +0000

    C P .py

---
+++ models/place.py
@@ -0,0 +1,39 @@
+#!usr/bin/python3
+import uuid
+import datetime
+import models
+
+
+class BaseModel:
+   
+    """Initialise class"""
+
+
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        models.storage.new(self)
+
+
+    """modify the updateAt attribute"""
+   
+    def save(self):
+        self.updated_at = datetime.now()
+
+
+    """return string representation"""
+   
+    def __str__(self):
+       return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+   
+    """return the dict"""
+
+
+    def to_dict(self):
+        obj = dict(self.__dict__)
+        obj["__class__"] = self.__class__.__name__
+        obj["created_at"] = obj["created_at"].isoformat()
+        obj["updated_at"] = obj["updated_at"].isoformat()
+        return obj
+
