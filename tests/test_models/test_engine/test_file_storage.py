8c4689be82debbc4b509d7baf574904ce9dbcc06
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-05-18 21:58:01 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import unittest
-from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 class TestBase(unittest.TestCase):
