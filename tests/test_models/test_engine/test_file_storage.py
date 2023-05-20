1ee1d3464a6d300284e5b1b589e615d9a9a1640c
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-05-20 13:04:58 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,9 +3,6 @@ import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
-storage.reload()
-
 class TestBase(unittest.TestCase):
     def test_class_properties(self):
         self.assertIsInstance(storage, FileStorage)
